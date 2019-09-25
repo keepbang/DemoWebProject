e503fc21cfd48386114440ac07f17809196dfd9c
채기병 <bang@ejbt.co.kr>
2019-09-25 16:35:27 +0900

    war배포를 위한 작업

--- bang_demo_project/src/main/java/com/bang/demo/BangDemoProjectApplication.java
+++ bang_demo_project/src/main/java/com/bang/demo/BangDemoProjectApplication.java
@@ -2,12 +2,19 @@ package com.bang.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BangDemoProjectApplication {
+public class BangDemoProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BangDemoProjectApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BangDemoProjectApplication.class);
 	}
 
 }
